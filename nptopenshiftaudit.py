e3f0121fa46fa3c69e8359779de70375346b2c0c
Brandon <bschran17@gmail.com>
2021-10-15 02:59:12 -0400

    removed tokens

--- nptopenshiftaudit.py
+++ nptopenshiftaudit.py
@@ -24,7 +24,7 @@ project_list = [
 ]
 
 login_commands = [
-    "x",
+    "x", # Paste login commands for each environment
     "x",
     "x",
     "x",
@@ -33,7 +33,7 @@ login_commands = [
 
 URL = "https://prometheus-njrar-02.paas.ups.com/api/v1/query"
 
-TOKEN = "Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6IiJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJvcGVuc2hpZnQtbW9uaXRvcmluZyIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VjcmV0Lm5hbWUiOiJwcm9tZXRoZXVzLWs4cy10b2tlbi1qNXFsOCIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50Lm5hbWUiOiJwcm9tZXRoZXVzLWs4cyIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50LnVpZCI6IjYxMjljOTg2LTA4MjMtMTFlYi05M2VhLTAwNTA1NmE1YTQ4MyIsInN1YiI6InN5c3RlbTpzZXJ2aWNlYWNjb3VudDpvcGVuc2hpZnQtbW9uaXRvcmluZzpwcm9tZXRoZXVzLWs4cyJ9.MPOL_QHBe1XK9xlc5mVZ0TG5r2sunnSVUbi_OFS4891KU4Zae0-MU6CmohAejoiLjSPsWdKj-SJPnvoYxa8UTjcTEtYoy8EvbSeBRIkW2CmwYUVt4ZDqfdG-UdsLRizquRdwtBZcqh4xAer-5uP5Zu4WToN1qTeY0JvMz-XIR9J482Mw3TcCT8La6TP7aLLOj8LOY6tlivQJ3PlLqE7Z5mDmm4v8hZ_bMWrHX95sB2c_piuCRjHXE9Vyz72C9ewJ6NrpcQR51KGj-HPon8V5AvGO6s7nsrHCCykt-dzKD1sF5rgGwIw_fRk0mqN3HbqrSRz2BIKyRx_iz4l3OeUqsA"
+TOKEN = "x" # Paste Bearer token
 
 QUERIES = [
     "avg_over_time(namespace_pod_name_container_name:container_cpu_usage_seconds_total:sum_rate{namespace='npt-prod', container_name!='POD'}[1d])",
